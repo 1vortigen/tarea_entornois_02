90fddf3191e78ad9a9ead5400dc570fa81142190
Adrian <nekovortigen@gmail.com>
2016-11-29 19:02:27 +0100

    pues eso

---
+++ saludo_informal/src/saludo_informal/Saludo_informal.java
@@ -0,0 +1,17 @@
+package saludo_informal;
+/**
+*
+* @author usuario
+*/
+public class Saludo_informal {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+       
+     System.out.print("hola gente");
+       
+   }
+   
+}
